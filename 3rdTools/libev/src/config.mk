37054e8744538912ba529fa2d66fd357c457ce59
lokiwang <wangjian1009@gmail.com>
2011-11-01 14:39:15 +0800

    init import libev 4.0.4

---
+++ 3rdTools/libev/src/config.mk
@@ -0,0 +1,14 @@
+product:=ev
+$(product).type:=clib
+$(product).libraries:=
+$(product).version:=4.0.4
+$(product).sources := $(addprefix $(product-base), \
+                          ev.c \
+                          event.c \
+                       )
+
+$(product).product.include:=3rdTools/libev/include
+$(product).flags.cpp:=-DHAVE_CONFIG_H -I$(product-base)$(dev-env)
+$(product).flags.ld:=
+
+$(eval $(call product-def,$(product)))
