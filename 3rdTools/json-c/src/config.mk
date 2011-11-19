9fcee0593405567c5f1fead75fe1335856fca740
lokiwang <wangjian1009@gmail.com>
2011-11-19 12:20:59 +0800

    json-c fix compile warning after added -Wall

--- 3rdTools/json-c/src/config.mk
+++ 3rdTools/json-c/src/config.mk
@@ -2,7 +2,7 @@ product:=json-c
 $(product).type:=clib
 $(product).sources:=$(wildcard $(product-base)*.c)
 $(product).product.include:=3rdTools/json-c/include
-$(product).flags.cpp:=-I$(product-base)/$(dev-env)
+$(product).flags.cpp:=-I$(product-base)/$(dev-env) -D_GNU_SOURCE
 $(product).flags.ld:=
 
 $(eval $(call product-def,$(product)))
