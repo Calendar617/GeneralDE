fe613d83e173ba86cf84f8a640c125766985deef
lokiwang <wangjian1009@gmail.com>
2011-11-23 11:57:51 +0800

    gtest set linker in config.mk

--- 3rdTools/gtest/src/config.mk
+++ 3rdTools/gtest/src/config.mk
@@ -5,5 +5,6 @@ $(product).product.c.includes:=3rdTools/gtest/include
 $(product).c.sources:=$(wildcard $(product-base)*.cc)
 $(product).c.includes:=3rdTools/gtest
 $(product).c.flags.ld:=-lpthread
+$(product).c.linker:=$(LINK.cc)
 
 $(eval $(call product-def,$(product)))
