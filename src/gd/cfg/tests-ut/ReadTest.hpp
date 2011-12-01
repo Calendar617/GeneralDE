fd27577a34fcd8a6e7964bf7b587e460e45af6bc
lokiwang <wangjian1009@gmail.com>
2011-12-01 20:38:37 +0800

    gd.cfg add first read test case, not success

---
+++ src/gd/cfg/tests-ut/ReadTest.hpp
@@ -0,0 +1,23 @@
+#ifndef CPE_DR_DATAJSON_TEST_PRINTTEST_H
+#define CPE_DR_DATAJSON_TEST_PRINTTEST_H
+#include <string.h>
+#include "gtest/gtest.h"
+#include "cpe/utils/error_list.h"
+#include "cpe/utils/buffer.h"
+#include "CfgTest.hpp"
+
+class ReadTest : public CfgTest {
+public:
+    ReadTest();
+    virtual void SetUp();
+    virtual void TearDown();
+
+    error_list_t m_errorList;
+
+    int read(const char * input);
+
+    struct mem_buffer m_result_buffer;
+    const char * result(void);
+};
+
+#endif
