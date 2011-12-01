1bb258fb8813d51715b52e5b926c079920809927
lokiwang <wangjian1009@gmail.com>
2011-12-01 14:31:43 +0800

    gd.cfg add seq testcases

--- src/gd/cfg/tests-ut/test_struct.cpp
+++ src/gd/cfg/tests-ut/test_struct.cpp
@@ -1,6 +1,8 @@
 #include "CfgTest.hpp"
 
-TEST_F(CfgTest, struct_add_basic) {
+class StructTest : public CfgTest {};
+
+TEST_F(StructTest, add_basic) {
     gd_cfg_t s = gd_cfg_struct_add_struct(m_root, "a");
     ASSERT_TRUE(s);
     EXPECT_STREQ("a", gd_cfg_name(s));
@@ -10,7 +12,17 @@ TEST_F(CfgTest, struct_add_basic) {
     EXPECT_TRUE(m_root == gd_cfg_parent(s));
 }
 
-TEST_F(CfgTest, struct_add_not_from_struct) {
+TEST_F(StructTest, add_seq) {
+    gd_cfg_t seq = gd_cfg_struct_add_seq(m_root, "a");
+    ASSERT_TRUE(seq);
+
+    EXPECT_EQ(GD_CFG_TYPE_SEQUENCE, gd_cfg_type(seq));
+    EXPECT_TRUE(m_root == gd_cfg_parent(seq));
+
+    EXPECT_EQ(0, gd_cfg_seq_count(seq));
+}
+
+TEST_F(StructTest, add_not_from_struct) {
     gd_cfg_t not_struct = gd_cfg_struct_add_int8(m_root, "a", 3);
     ASSERT_TRUE(not_struct);
 
@@ -18,22 +30,22 @@ TEST_F(CfgTest, struct_add_not_from_struct) {
     ASSERT_FALSE(s);
 }
 
-TEST_F(CfgTest, struct_find_empty) {
+TEST_F(StructTest, find_empty) {
     EXPECT_FALSE(gd_cfg_struct_find_cfg(m_root, "a"));
 }
 
-TEST_F(CfgTest, struct_find_success) {
+TEST_F(StructTest, find_success) {
     gd_cfg_t s = gd_cfg_struct_add_struct(m_root, "a");
     ASSERT_TRUE(s);
 
     EXPECT_TRUE(s == gd_cfg_struct_find_cfg(m_root, "a"));
 }
 
-TEST_F(CfgTest, struct_find_not_from_struct) {
+TEST_F(StructTest, find_not_from_struct) {
     EXPECT_FALSE(gd_cfg_struct_find_cfg(gd_cfg_struct_add_seq(m_root, "a"), ""));
 }
 
-TEST_F(CfgTest, struct_free_child) {
+TEST_F(StructTest, free_child) {
     gd_cfg_t s = gd_cfg_struct_add_struct(m_root, "a");
     ASSERT_TRUE(s);
 
