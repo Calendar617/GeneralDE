2742e287d6e0239ced2cfe4c35810b09e42ec0c5
lokiwang <wangjian1009@gmail.com>
2011-12-08 16:56:19 +0800

    cpe.utils add file_name_base

--- include/cpe/utils/file.h
+++ include/cpe/utils/file.h
@@ -36,7 +36,10 @@ ssize_t file_stream_write_from_stream(FILE * fp, read_stream_t stream, error_mon
 ssize_t file_stream_load_to_buf(char * buf, size_t size, FILE * fp, error_monitor_t em);
 ssize_t file_stream_load_to_buffer(mem_buffer_t buffer, FILE * fp, error_monitor_t em);
 ssize_t file_stream_load_to_stream(write_stream_t stream, FILE * fp, error_monitor_t em);
-const char * file_suffix(const char * input);
+
+/*file name operations*/
+const char * file_name_suffix(const char * input);
+const char * file_name_base(const char * input, mem_buffer_t tbuf);
 
 /*dir operations*/
 int dir_exist(const char * path, error_monitor_t em);
