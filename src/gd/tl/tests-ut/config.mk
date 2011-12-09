1f1aae9f58789808328b649493382240f0c05858
lokiwang <wangjian1009@gmail.com>
2011-12-09 13:10:28 +0800

    gd.tl tests-ut use gtest-def macro

--- src/gd/tl/tests-ut/config.mk
+++ src/gd/tl/tests-ut/config.mk
@@ -1,9 +1,1 @@
-product:=gtest.gd_tl
-$(product).type:=progn
-$(product).buildfor:=dev
-$(product).depends:=gtest gd_tl cpe_utils
-$(product).c.sources := $(wildcard $(product-base)*.cpp)
-$(product).run.path:=$(product-base)
-gd_tl.ut:=$(product)
-
-$(eval $(call product-def,$(product)))
+$(eval $(call gtest-def,gd_tl))
