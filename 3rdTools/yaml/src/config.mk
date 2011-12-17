878d05375b5e9cd011b1a0848cb919ce21d3cfc2
Loki Wang <wangjian1009@gmail.com>
2011-12-17 20:46:31 +0800

    yaml config.mk update for buildtools change

--- 3rdTools/yaml/src/config.mk
+++ 3rdTools/yaml/src/config.mk
@@ -1,7 +1,7 @@
 product:=yaml
 $(product).type:=lib
 $(product).product.c.includes:=3rdTools/yaml/include
-$(product).c.sources:=$(wildcard $(product-base)*.c)
+$(product).c.sources:=$(wildcard $(product-base)/*.c)
 $(product).c.includes:=3rdTools/yaml/src/$(dev-env)
 $(product).c.flags.cpp:=-DHAVE_CONFIG_H
 $(product).c.flags.c:= -O2
