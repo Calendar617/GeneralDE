498c88fa7f42475ee83fd9316948917ea339ce8f
Loki Wang <wangjian1009@gmail.com>
2011-12-17 20:41:31 +0800

    json-c config.mk update for buildtools change

--- 3rdTools/json-c/src/config.mk
+++ 3rdTools/json-c/src/config.mk
@@ -1,7 +1,7 @@
 product:=json-c
 $(product).type:=lib
 $(product).product.c.includes:=3rdTools/json-c/include
-$(product).c.sources:=$(wildcard $(product-base)*.c)
+$(product).c.sources:=$(wildcard $(product-base)/*.c)
 $(product).c.includes:=3rdTools/json-c/src/$(dev-env)
 $(product).c.flags.cpp:=-D_GNU_SOURCE
 $(product).c.flags.ld:=
