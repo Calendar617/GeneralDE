f11cd04f8fae9a3aaaa867b111f4cfacaff7601d
Loki Wang <wangjian1009@gmail.com>
2011-12-17 20:49:07 +0800

    dr2 config.mk update for buildtools change

--- src/cpe/dr/data-json/config.mk
+++ src/cpe/dr/data-json/config.mk
@@ -1,6 +1,6 @@
 product:=cpe_dr_data_json
 $(product).type:=lib
 $(product).depends:=cpe_dr cpe_utils yajl
-$(product).c.sources:=$(wildcard $(product-base)*.c)
+$(product).c.sources:=$(wildcard $(product-base)/*.c)
 
 $(eval $(call product-def,$(product)))
--- src/cpe/utils/config.mk
+++ src/cpe/utils/config.mk
@@ -2,6 +2,6 @@ product:=cpe_utils
 $(product).type:=lib
 $(product).product.c.includes:=include
 $(product).libraries:=
-$(product).c.sources:=$(wildcard $(product-base)*.c)
+$(product).c.sources:=$(wildcard $(product-base)/*.c)
 
 $(eval $(call product-def,$(product)))
