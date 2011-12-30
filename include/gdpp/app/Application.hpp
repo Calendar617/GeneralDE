299c5d4ed7f5bd0895bc4e28396e58ecd3ca7d70
lokiwang <wangjian1009@gmail.com>
2011-12-30 13:12:44 +0800

    gdpp.app Application add allocrator function

--- include/gdpp/app/Application.hpp
+++ include/gdpp/app/Application.hpp
@@ -10,6 +10,8 @@ class Application : public Cpe::Utils::SimulateObject {
 public:
     int argc(void) const { return gd_app_argc(*this); }
 
+    mem_allocrator_t allocrator(void) { return gd_app_alloc(*this); }
+
     Nm::Manager & nmManager(void) { return *((Nm::Manager *)gd_app_nm_mgr(*this)); }
 
     operator gd_app_context_t () const { return (gd_app_context_t)(void*)this; }
