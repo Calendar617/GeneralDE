c9e5d06bcc31b662fcd1f8582057655eb4acdbcb
lokiwang <wangjian1009@gmail.com>
2011-12-30 18:28:28 +0800

    cpepp.utils OpGuard support MemFun_1

--- include/cpepp/utils/OpGuard.hpp
+++ include/cpepp/utils/OpGuard.hpp
@@ -41,6 +41,32 @@ public:
                 new OpGuardNode_MemFun<T>(o, fun)));
     }
 
+    template<typename T, typename ArgT1>
+    class OpGuardNode_MemFun_1 : public OpGuardNode {
+    public:
+        OpGuardNode_MemFun_1(T & o, void (T::*fun)(ArgT1 arg), ArgT1 arg1)
+            : m_o(o)
+            , m_fun(fun)
+            , m_arg1(arg1) 
+        {
+        }
+
+        virtual void execute(void) {
+            (m_o.*m_fun)(m_arg1);
+        }
+
+        T & m_o;
+        void (T::*m_fun)(void);
+        ArgT1 m_arg1;
+    };
+
+    template<typename T, typename ArgT1>
+    void addOp(T & o, void (T::*fun)(ArgT1), ArgT1 arg1) {
+        this->addOpNode(
+            ::std::auto_ptr<OpGuardNode>(
+                new OpGuardNode_MemFun_1<T, ArgT1>(o, fun, arg1)));
+    }
+
     void releaseControl(void);
 
 private:
