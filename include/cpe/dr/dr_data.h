625c564d449830795512cf2417c3f72fc9ce26ae
lokiwang <wangjian1009@gmail.com>
2012-01-04 12:40:19 +0800

    dr_data.h remove a not impl func

--- include/cpe/dr/dr_data.h
+++ include/cpe/dr/dr_data.h
@@ -18,8 +18,6 @@ int dr_entry_try_read_uint32(uint32_t * result, const void * input, LPDRMETAENTR
 int dr_entry_try_read_int64(int64_t * result, const void * input, LPDRMETAENTRY entry, error_monitor_t em);
 int dr_entry_try_read_uint64(uint64_t * result, const void * input, LPDRMETAENTRY entry, error_monitor_t em);
 
-int dr_entry_write_from_string(void * output, LPDRMETAENTRY entry, const char * input, error_monitor_t em);
-
 int8_t dr_entry_read_int8(const void * input, LPDRMETAENTRY entry);
 uint8_t dr_entry_read_uint8(const void * input, LPDRMETAENTRY entry);
 int16_t dr_entry_read_int16(const void * input, LPDRMETAENTRY entry);
