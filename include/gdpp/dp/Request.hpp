0649d9252f20a84215f53a409adc9d40e5fe8212
lokiwang <wangjian1009@gmail.com>
2012-01-05 22:34:08 +0800

    gdpp.dp Request add mgr and _free function

--- include/gdpp/dp/Request.hpp
+++ include/gdpp/dp/Request.hpp
@@ -11,8 +11,17 @@ public:
     operator gd_dp_req_t () const { return (gd_dp_req_t)this; }
 
     void * data(void) { return gd_dp_req_data((gd_dp_req_t)this); }
+
     size_t size(void) { return gd_dp_req_size((gd_dp_req_t)this); }
+    void setSize(size_t size) { gd_dp_req_set_size(*this, size); }
+
+    size_t capacity(void) { return gd_dp_req_capacity((gd_dp_req_t)this); }
+
     mem_allocrator_t talloc(void) { return gd_dp_req_talloc((gd_dp_req_t)this); }
+
+    gd_dp_mgr_t mgr() {
+        return gd_dp_req_getMgr( (gd_dp_req_t)this );
+    }
 
     void setParent(gd_dp_req_t parent) {
         gd_dp_req_set_parent(*this, parent);
@@ -58,6 +67,10 @@ public:
     static Request * _create(gd_dp_mgr_t mgr, cpe_hash_string_t type, char * buf, size_t capacity) {
         return (Request*)gd_dp_req_create_with_buf(mgr, type, buf, capacity);
     }
+
+    static Request & _cast(gd_dp_req_t req);
+
+    static void _free(Request * req) { if (req) { gd_dp_req_free(*req); } }
 
     template<typename T>
     T & as(void) { checkSize(sizeof(T)); return *((T*)gd_dp_req_data((gd_dp_req_t)this)); }
