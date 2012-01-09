b1da0a3fb8ddeb60f34007b6650bad787d58d834
Lokiwang <wangjian1009@gmail.com>
2012-01-09 15:46:33 +0800

    gdpp.evt Event  add utils function

--- include/gdpp/evt/Event.hpp
+++ include/gdpp/evt/Event.hpp
@@ -1,6 +1,7 @@
 #ifndef GDPP_EVT_EVENT_H
 #define GDPP_EVT_EVENT_H
 #include "cpepp/utils/ClassCategory.hpp"
+#include "cpepp/utils/CString.hpp"
 #include "cpepp/dr/Meta.hpp"
 #include "cpepp/dr/Data.hpp"
 #include "gd/evt/evt_read.h"
@@ -12,7 +13,7 @@ class Event : public Cpe::Utils::SimulateObject {
 public:
     operator gd_evt_t (void) const { return (gd_evt_t)this; }
 
-    const char * type(void) const { return gd_evt_type(*this); }
+    Cpe::Utils::CString const & type(void) const { return Cpe::Utils::CString::_cast(gd_evt_type(*this)); }
 
     Cpe::Dr::Meta const & meta(void) const {
         return Cpe::Dr::Meta::_cast(gd_evt_meta(*this));
@@ -27,6 +28,14 @@ public:
 
     Cpe::Dr::Data args(void) {
         return Cpe::Dr::Data(gd_evt_data(*this), meta());
+    }
+
+    Cpe::Dr::ConstDataElement operator[] (const char * name) const {
+        return args()[name];
+    }
+
+    Cpe::Dr::DataElement operator[] (const char * name) {
+        return args()[name];
     }
 
     static Event & _cast(gd_evt_t evt);
