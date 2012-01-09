ba8e50c10a587c52da28890dd8c120e2383690c8
lokiwang <wangjian1009@gmail.com>
2012-01-09 14:40:07 +0800

    cpepp.dr MetaLib load function complete

---
+++ include/cpepp/utils/System.hpp
@@ -0,0 +1,14 @@
+#ifndef CPEPP_UTILS_SYSTEM_H
+#define CPEPP_UTILS_SYSTEM_H
+
+namespace Cpe { namespace Utils {
+
+class MemBuffer;
+class OpGuard;
+class CString;
+class RangeMgr;
+
+}}
+
+#endif
+
