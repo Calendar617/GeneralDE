7e9e6ae3626217346bbd1ab56123825a8f5aa846
lokiwang <wangjian1009@gmail.com>
2012-01-09 13:48:59 +0800

    cpepp.dr add some function

--- include/cpepp/dr/MetaLib.hpp
+++ include/cpepp/dr/MetaLib.hpp
@@ -11,7 +11,13 @@ class MetaLib : public Cpe::Utils::SimulateObject {
 public:
     operator LPDRMETALIB (void) const { return (LPDRMETALIB)this; }
 
-    static MetaLib const & _cast(const void * p, size_t size);
+    Meta const * findMeta(const char * name) const { return (Meta const *)dr_lib_find_meta_by_name(*this, name); }
+    Meta const & meta(const char * name);
+
+    Meta const * findMeta(int id) const  { return (Meta const *)dr_lib_find_meta_by_id(*this, id); }
+    Meta const & meta(int id) const;
+
+    static MetaLib const & _cast(const void * p);
     static MetaLib const & _cast(LPDRMETALIB ml);
 
     static MetaLib const & _load_from_file(const char * file, ::std::vector<char> & buf);
