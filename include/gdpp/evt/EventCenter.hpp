86f894e1d12d719b1673afe4f6206541e423b7b2
Lokiwang <wangjian1009@gmail.com>
2012-01-13 16:03:36 +0800

    gdpp.evt EventCenter leak sendEvent

--- include/gdpp/evt/EventCenter.hpp
+++ include/gdpp/evt/EventCenter.hpp
@@ -14,6 +14,7 @@ public:
     Cpe::Dr::MetaLib const & metaLib(void) const { return *(Cpe::Dr::MetaLib*)gd_evt_mgr_metalib(*this); }
 
     Event & createEvent(size_t attach_capacity, const char * name, ssize_t data_capacity = -1);
+    void sendEvent(Event & event);
 
     static EventCenter & _cast(gd_evt_mgr_t evm);
 };
