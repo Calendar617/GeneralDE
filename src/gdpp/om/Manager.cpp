0b94cb990a0efaaa2fafc16d0b6da3be5de26839
lokiwang <wangjian1009@gmail.com>
2012-01-18 15:23:53 +0800

    gdpp.om Manager alloc and free complete

--- src/gdpp/om/Manager.cpp
+++ src/gdpp/om/Manager.cpp
@@ -2,19 +2,38 @@
 #include <sstream>
 #include <stdexcept>
 #include "cpepp/utils/ErrorCollector.hpp"
+#include "gd/om/om_object.h"
 #include "gdpp/om/Manager.hpp"
 
 namespace Gd { namespace Om {
 
 void * Manager::alloc(cpe_hash_string_t className) {
-    return NULL;
+    gd_om_oid_t oid = gd_om_obj_alloc(*this, className, NULL);
+    if (oid == GD_OM_INVALID_OID) {
+        throw ::std::bad_alloc();
+    }
+
+    void * p = gd_om_obj_get(*this, oid, NULL);
+    if (p == NULL) {
+        throw ::std::runtime_error("OmManager: get buf from oid fail!");
+    }
+
+    return p;
 }
 
 void * Manager::alloc_nothrow(cpe_hash_string_t className) {
-    return NULL;
+    gd_om_oid_t oid = gd_om_obj_alloc(*this, className, NULL);
+    if (oid == GD_OM_INVALID_OID) {
+        return NULL;
+    }
+
+    return gd_om_obj_get(*this, oid, NULL);
 }
 
 void Manager::free(Object * p) {
+    gd_om_oid_t oid = gd_om_obj_id_from_addr(*this, (void*)p, NULL);
+    assert(oid != GD_OM_INVALID_OID);
+    gd_om_obj_free(*this, oid, NULL);
 }
 
 void Manager::registClass(const char * className, size_t object_size, size_t align) {
