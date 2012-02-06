6c728d519530a6b7d17910adaa506e8c52246583
lokiwang <wangjian1009@gmail.com>
2012-02-06 14:47:19 +0800

    cpe.cfg add merge function, not impl

---
+++ src/cpe/cfg/cfg_merge.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include "cpe/cfg/cfg_manage.h"
+
+static void cfg_merge_i(cfg_t cfg, cfg_t input, cfg_policy_t policy, error_monitor_t em) {
+    if (cfg->m_type == CPE_CFG_TYPE_STRUCT) {
+        if (input->m_type == CPE_CFG_TYPE_STRUCT) {
+            struct cfg_it_t it;
+            cfg_t child;
+
+            cfg_it_init(&it, input);
+            while((child = cfg_it_next(&it))) {
+                
+            }
+        }
+        else {
+        }
+    }
+    else if (cfg->m_type == CPE_CFG_TYPE_SEQUENCE) {
+    }
+    else {
+    }
+}
+
+int cfg_merge(cfg_t cfg, cfg_t input, cfg_policy_t policy, error_monitor_t em) {
+    assert(cfg);
+    if (input == NULL) return 0;
+
+    cfg_merge_i(cfg, input, policy, em);
+
+    return 0;
+}
