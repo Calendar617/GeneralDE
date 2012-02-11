956e3964793ad7ceb81f63d6ff4c17f2a2afbaa3
Lokiwang <wangjian1009@gmail.com>
2012-02-11 08:44:36 +0800

    cpe.dr dr_cfg add dr_cfg_write code, not test

--- src/cpe/dr/data-cfg/tests-ut/WriteTest.hpp
+++ src/cpe/dr/data-cfg/tests-ut/WriteTest.hpp
@@ -1,5 +1,5 @@
-#ifndef CPE_DR_DATACFG_TEST_READTEST_H
-#define CPE_DR_DATACFG_TEST_READTEST_H
+#ifndef CPE_DR_DATACFG_TEST_WRITETEST_H
+#define CPE_DR_DATACFG_TEST_WRITETEST_H
 #include "cpe/utils/tests-env/test-fixture.hpp"
 #include "cpe/utils/tests-env/with_em.hpp"
 #include "cpe/cfg/tests-env/with_cfg.hpp"
@@ -7,22 +7,21 @@
 
 typedef LOKI_TYPELIST_2(
     utils::testenv::with_em,
-    cpe::cfg::testenv::with_cfg) ReadTestBase;
+    cpe::cfg::testenv::with_cfg) WriteTestBase;
 
-class ReadTest : public testenv::fixture<ReadTestBase> {
+class WriteTest : public testenv::fixture<WriteTestBase> {
 public:
-    ReadTest();
+    WriteTest();
     virtual void SetUp();
     virtual void TearDown();
 
     LPDRMETALIB m_metaLib;
     struct mem_buffer m_metaLib_buffer;
 
-    struct mem_buffer m_buffer;
+    cfg_t m_cfg;
 
     void installMeta(const char * data);
-    int read(const char * data, const char * typeName, int policy = 0, size_t capacity = 0);
-    void * result(int startPos = 0);
+    int write(const char * data, const char * typeName);
 };
 
 #endif
