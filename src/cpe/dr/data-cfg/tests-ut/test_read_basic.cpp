ddcdb0de7d96f965e367efdeeffdbe62223273b3
Lokiwang <wangjian1009@gmail.com>
2012-02-13 22:06:21 +0800

    cpe.dr and cpepp.dr bug fix

--- src/cpe/dr/data-cfg/tests-ut/test_read_basic.cpp
+++ src/cpe/dr/data-cfg/tests-ut/test_read_basic.cpp
@@ -95,3 +95,46 @@ TEST_F(ReadTest, seq_size_dft_no_refer) {
     EXPECT_EQ(34, dr_ctype_read_int16(result(24), CPE_DR_TYPE_INT16));
 }
 
+TEST_F(ReadTest, int8_basic) {
+    t_em_set_print();
+
+    installMeta(
+        "<metalib tagsetversion='1' name='net'  version='1'>"
+        "    <struct name='S' version='1'>"
+        "	     <entry name='a1' type='int8'/>"
+        "	     <entry name='a2' type='int8'/>"
+        "    </struct>"
+        "</metalib>");
+
+    EXPECT_EQ(
+        2,
+        read(
+            "a1: 12\n"
+            "a2: 14\n",
+            "S"));
+
+    EXPECT_EQ(12, (int)dr_ctype_read_int8(result(), CPE_DR_TYPE_INT8));
+    EXPECT_EQ(14, (int)dr_ctype_read_int8(result(1), CPE_DR_TYPE_INT8));
+}
+
+TEST_F(ReadTest, uint8_basic) {
+    t_em_set_print();
+
+    installMeta(
+        "<metalib tagsetversion='1' name='net'  version='1'>"
+        "    <struct name='S' version='1'>"
+        "	     <entry name='a1' type='uint8'/>"
+        "	     <entry name='a2' type='uint8'/>"
+        "    </struct>"
+        "</metalib>");
+
+    EXPECT_EQ(
+        2,
+        read(
+            "a1: 0\n"
+            "a2: 14\n",
+            "S"));
+
+    EXPECT_EQ(0, (int)dr_ctype_read_uint8(result(), CPE_DR_TYPE_UINT8));
+    EXPECT_EQ(14, (int)dr_ctype_read_uint8(result(1), CPE_DR_TYPE_UINT8));
+}
