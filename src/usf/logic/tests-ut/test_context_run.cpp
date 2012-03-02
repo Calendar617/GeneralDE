fed224a5818b5ec745cb90d717c005d593004d91
Lokiwang <wangjian1009@gmail.com>
2012-03-02 10:56:52 +0800

    usf.logic add group multi level test

--- src/usf/logic/tests-ut/test_context_run.cpp
+++ src/usf/logic/tests-ut/test_context_run.cpp
@@ -111,6 +111,35 @@ TEST_F(ContextRunTest, group_multi_error_break) {
     logic_executor_free(executor);
 }
 
+TEST_F(ContextRunTest, group_multi_level_error_break) {
+    LogicOpMock & op1 = installOp("Op1");
+    installOp("Op2");
+    installOp("Op3");
+    installOp("Op4");
+
+    EXPECT_CALL(op1, execute(::testing::_))
+        .WillOnce(::testing::Return(123));
+
+    logic_context_t context = t_logic_context_create();
+
+    logic_executor_t executor =
+        t_logic_executor_build(
+            "-\n"
+            "  -\n"
+            "    - Op1\n"
+            "    - Op2\n"
+            "  - Op3\n"
+            "- Op4"
+            );
+
+    t_logic_execute(context, executor);
+
+    EXPECT_EQ(logic_context_error, logic_context_state(context));
+    EXPECT_EQ((int32_t)123, logic_context_errno(context));
+
+    logic_executor_free(executor);
+}
+
 TEST_F(ContextRunTest, protected_basic) {
     LogicOpMock & op1 = installOp("Op1");
 
