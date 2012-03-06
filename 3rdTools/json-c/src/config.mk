2ff84ed61bbb0759ccaf32ac93385ac51cbd70e5
Lokiwang <wangjian1009@gmail.com>
2012-03-06 10:11:28 +0800

    json-c update config.mk for support domain

--- 3rdTools/json-c/src/config.mk
+++ 3rdTools/json-c/src/config.mk
@@ -2,7 +2,7 @@ product:=json-c
 $(product).type:=lib
 $(product).product.c.includes:=3rdTools/json-c/include
 $(product).c.sources:=$(wildcard $(product-base)/*.c)
-$(product).c.includes:=3rdTools/json-c/src/$(dev-env)
+$(product).c.env-includes:=3rdTools/json-c/src
 $(product).c.flags.cpp:=-D_GNU_SOURCE
 $(product).c.flags.ld:=
 
