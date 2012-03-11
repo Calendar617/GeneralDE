fc90c6839cbf4a4f984d859af74245609db88389
Lokiwang <wangjian1009@gmail.com>
2012-03-11 21:48:45 +0800

    product-def-cpe-dr.mk bug fix

--- buildtools/detail/product-def-cpe-dr.mk
+++ buildtools/detail/product-def-cpe-dr.mk
@@ -19,7 +19,7 @@ define product-def-rule-cpe-dr-c-module-h
   $(call c-source-to-object,$(r.$1.c.sources),$3): $(r.$1.$3.cpe-dr.$2.generated.h)
 
   $(foreach source,$(r.$1.$3.cpe-dr.$2.source),\
-      $(r.$1.$3.cpe-dr.$2.h.output-dir)/$(patsubst %.xml,%.h,$(notdir $(source))): $(source) $(cpe-dr-tool))
+      $(r.$1.$3.cpe-dr.$2.h.output-dir)/$(patsubst %.xml,%.h,$(notdir $(source)))): $(source) $(cpe-dr-tool)
 
   $(r.$1.$3.cpe-dr.$2.generated.h): $(r.$1.$3.cpe-dr.$2.source) 
 	$$(call with_message,cpe-dr generaing h to $(subst $(CPDE_ROOT)/,,$(r.$1.$3.cpe-dr.$2.h.output-dir)) ...) \
@@ -74,20 +74,19 @@ define product-def-rule-cpe-dr-c-module-bin
 endef
 
 define product-def-rule-cpe-dr-c-module
-  $(call assert-not-null,$1.cpe-dr.$3.source)
-  $(call assert-not-null,$1.cpe-dr.$3.generate)
+$(call assert-not-null,$1.cpe-dr.$3.source)
+$(call assert-not-null,$1.cpe-dr.$3.generate)
 
-  $(eval r.$1.$2.cpe-dr.$3.source:=$($1.cpe-dr.$3.source))
-  $(eval r.$1.$2.cpe-dr.$3.generate:=$($1.cpe-dr.$3.generate))
+$(eval r.$1.$2.cpe-dr.$3.source:=$($1.cpe-dr.$3.source))
+$(eval r.$1.$2.cpe-dr.$3.generate:=$($1.cpe-dr.$3.generate))
 
-  $(foreach p,$(r.$1.$2.cpe-dr.$3.generate),\
-    $(call product-def-rule-cpe-dr-c-module-$p,$1,$3,$2))
+$(foreach p,$(r.$1.$2.cpe-dr.$3.generate), $(call product-def-rule-cpe-dr-c-module-$p,$1,$3,$2))
 
 endef
 
 define product-def-rule-cpe-dr
 
- $(foreach module,$(r.$1.cpe-dr.modules),\
+$(foreach module,$(r.$1.cpe-dr.modules),\
 	$(call product-def-rule-cpe-dr-c-module,$1,$2,$(module)))
 
 endef
