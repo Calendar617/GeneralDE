a9b903e5c1097077ca5418c01ed336b94560ece6
Lokiwang <wangjian1009@gmail.com>
2012-03-15 12:10:56 +0800

    remove not used vnet

--- include/gd/vnet/vnet.h
+++
@@ -1,7 +0,0 @@
-#ifndef GD_VNET_ALL_H
-#define GD_VNET_ALL_H
-
-#include "vnet_types.h"
-#include "vnet_manage.h"
-
-#endif
--- src/gd/vnet/config.mk
+++
@@ -1,7 +0,0 @@
-product:=gd_vnet
-$(product).type:=lib
-$(product).depends:=cpe_utils cpe_cfg gd_app
-$(product).c.flags.ld:=
-$(product).c.sources:=$(wildcard $(product-base)/*.c)
-
-$(eval $(call product-def,$(product)))
