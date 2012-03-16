d53b8e24f669d6a4d43f5536b006db733f07d5b6
Lokiwang <wangjian1009@gmail.com>
2012-03-16 17:17:23 +0800

    usf.bpg_net bug fix, begin read data

--- src/usf/bpg_net/bpg_net_agent_process.c
+++ src/usf/bpg_net/bpg_net_agent_process.c
@@ -2,6 +2,7 @@
 #include "cpe/dr/dr_metalib_manage.h"
 #include "cpe/dr/dr_data.h"
 #include "cpe/dr/dr_cvt.h"
+#include "cpe/net/net_chanel.h"
 #include "cpe/net/net_endpoint.h"
 #include "gd/dp/dp_manage.h"
 #include "gd/app/app_context.h"
@@ -16,6 +17,12 @@ static int s_totallen_end_pos = 0;
 
 static void bpg_net_agent_on_read(bpg_net_agent_t agent, net_ep_t ep) {
     bpg_req_t req_buf;
+
+    if(agent->m_debug) {
+        CPE_INFO(
+            agent->m_em, "%s: ep %d: on read",
+            bpg_net_agent_name(agent), (int)net_ep_id(ep));
+    }
 
     req_buf = bpg_net_agent_req_buf(agent);
     if (req_buf == NULL) {
@@ -54,6 +61,7 @@ static void bpg_net_agent_on_read(bpg_net_agent_t agent, net_ep_t ep) {
         ssize_t package_decode_size;
 
         buf_size = net_ep_size(ep);
+
         if (buf_size < s_totallen_end_pos) break;
 
         buf = net_ep_peek(ep, NULL, buf_size);
@@ -97,6 +105,13 @@ static void bpg_net_agent_on_read(bpg_net_agent_t agent, net_ep_t ep) {
             break;
         }
 
+        if(agent->m_debug) {
+            CPE_ERROR(
+                agent->m_em, "%s: ep %d: read one request, cmd=%d, input-size=%d, package-size=%d!",
+                bpg_net_agent_name(agent), (int)net_ep_id(ep),
+                bpg_req_cmd(req_buf), (int)package_input_size, (int)package_decode_size);
+        }
+
         if (gd_dp_dispatch_by_numeric(bpg_req_cmd(req_buf), bpg_req_to_dp_req(req_buf), agent->m_em) != 0) {
             CPE_ERROR(
                 agent->m_em, "%s: ep %d: dispatch cmd %d error!",
@@ -106,10 +121,20 @@ static void bpg_net_agent_on_read(bpg_net_agent_t agent, net_ep_t ep) {
 }
 
 static void bpg_net_agent_on_open(bpg_net_agent_t agent, net_ep_t ep) {
-    
+    if(agent->m_debug) {
+        CPE_INFO(
+            agent->m_em, "%s: ep %d: on open",
+            bpg_net_agent_name(agent), (int)net_ep_id(ep));
+    }
+
 }
 
 static void bpg_net_agent_on_close(bpg_net_agent_t agent, net_ep_t ep, net_ep_event_t event) {
+    if(agent->m_debug) {
+        CPE_INFO(
+            agent->m_em, "%s: ep %d: on close, event=%d",
+            bpg_net_agent_name(agent), (int)net_ep_id(ep), event);
+    }
 }
 
 static void bpg_net_agent_process(net_ep_t ep, void * ctx, net_ep_event_t event) {
@@ -130,11 +155,62 @@ static void bpg_net_agent_process(net_ep_t ep, void * ctx, net_ep_event_t event)
     }
 }
 
-void bpg_net_agent_accept(net_listener_t listener, net_ep_t ep, void * ctx) {
+static void bpg_net_agent_free_chanel_buf(net_chanel_t chanel, void * ctx) {
     bpg_net_agent_t agent = (bpg_net_agent_t)ctx;
 
     assert(agent);
 
+    mem_free(agent->m_alloc, net_chanel_queue_buf(chanel));
+}
+
+
+void bpg_net_agent_accept(net_listener_t listener, net_ep_t ep, void * ctx) {
+    bpg_net_agent_t agent = (bpg_net_agent_t)ctx;
+    void * buf_r = NULL;
+    void * buf_w = NULL;
+    net_chanel_t chanel_r = NULL;
+    net_chanel_t chanel_w = NULL;
+
+    assert(agent);
+
+    buf_r = mem_alloc(agent->m_alloc, 2048);
+    buf_w = mem_alloc(agent->m_alloc, 2048);
+    if (buf_r == NULL || buf_w == NULL) goto ERROR;
+
+    chanel_r = net_chanel_queue_create(net_ep_mgr(ep), buf_r, 2048);
+    if (chanel_r == NULL) goto ERROR;
+    net_chanel_queue_set_close(chanel_r, bpg_net_agent_free_chanel_buf, agent);
+    buf_r = NULL;
+
+    chanel_w = net_chanel_queue_create(net_ep_mgr(ep), buf_w, 2048);
+    if (chanel_w == NULL) goto ERROR;
+    net_chanel_queue_set_close(chanel_w, bpg_net_agent_free_chanel_buf, agent);
+    buf_w = NULL;
+
+    net_ep_set_chanel_r(ep, chanel_r);
+    chanel_r = NULL;
+
+    net_ep_set_chanel_w(ep, chanel_w);
+    chanel_w = NULL;
+
     net_ep_set_processor(ep, bpg_net_agent_process, agent);
+
+    if(agent->m_debug) {
+        CPE_INFO(
+            agent->m_em, "%s: ep %d: accept success!",
+            bpg_net_agent_name(agent), (int)net_ep_id(ep));
+    }
+
+    return;
+ERROR:
+    if (buf_r) mem_free(agent->m_alloc, buf_r);
+    if (buf_w) mem_free(agent->m_alloc, buf_w);
+    if (chanel_r) net_chanel_free(chanel_r);
+    if (chanel_w) net_chanel_free(chanel_w);
+    net_ep_close(ep);
+
+    CPE_ERROR(
+        agent->m_em, "%s: ep %d: accept fail!",
+        bpg_net_agent_name(agent), (int)net_ep_id(ep));
 }
 
