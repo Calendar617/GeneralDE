d3c90fb7f0fb102f63e317b9a26ac1af70dee411
Lokiwang <wangjian1009@gmail.com>
2012-03-19 11:29:45 +0800

    resolve cpe.dr tools link error on linux32

--- src/cpe/dr/tools/config.mk
+++ src/cpe/dr/tools/config.mk
@@ -1,6 +1,6 @@
 product:=cpe_dr_tool_lib
 $(product).type:=lib 
-$(product).depends:=cpe_dr cpe_dr_meta_inout argtable2
+$(product).depends:=argtable2 cpe_dr_meta_inout cpe_dr 
 $(product).c.libraries:=
 $(product).c.sources:= $(filter-out %/main.c,$(wildcard $(product-base)/*.c))
 $(product).c.lib.type:=static 
@@ -10,4 +10,5 @@ product:=cpe_dr_tool
 $(product).type:=progn
 $(product).depends:=cpe_dr_tool_lib
 $(product).c.sources:=$(product-base)/main.c
+$(product).c.flags.ld:=-lcpe_dr -lcpe_dr_meta_inout -lxml2 -lm
 $(eval $(call product-def,$(product),tools))
