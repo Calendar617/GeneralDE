284926499a79f46abe2312aacb6f637a7b84498e
Lokiwang <wangjian1009@gmail.com>
2012-04-01 11:34:01 +0800

    move usf.dm_role to gd.dr_dm

--- src/usfpp/dm_role/config.mk
+++
@@ -1,7 +0,0 @@
-product:=usfpp_dm_role
-$(product).type:=lib
-$(product).depends:=cpepp_utils gdpp_app usf_dm_role
-$(product).c.flags.ld:=
-$(product).c.sources:=$(wildcard $(product-base)/*.cpp)
-
-$(eval $(call product-def,$(product)))
