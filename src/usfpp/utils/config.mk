e50a1643493e45e290c7035604e7c42630fd13dc
Lokiwang <wangjian1009@gmail.com>
2012-04-05 10:12:18 +0800

    move utils from usf to gd

--- src/usfpp/utils/config.mk
+++
@@ -1,7 +0,0 @@
-product:=usfpp_utils
-$(product).type:=lib
-$(product).depends:=cpepp_utils gdpp_app usf_utils
-$(product).c.flags.ld:=
-$(product).c.sources:=$(wildcard $(product-base)/*.cpp)
-
-$(eval $(call product-def,$(product)))
