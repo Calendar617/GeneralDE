5b44e6212dae8a55da77db6239e72a344388959f
Lokiwang <wangjian1009@gmail.com>
2012-05-15 21:12:58 +0800

    add mmseg basic test

--- 3rdTools/mmseg/src/tests-ut/MMSegTest.cpp
+++ 3rdTools/mmseg/src/tests-ut/MMSegTest.cpp
@@ -5,5 +5,5 @@ MMSegTest::MMSegTest() {
 
 void MMSegTest::SetUp() {
     ::testing::Test::SetUp();
-    EXPECT_EQ(0, m_segmentManage.init("data"));
+    EXPECT_EQ(0, m_segmentManage.init("data/"));
 }
--- 3rdTools/mmseg/src/tests-ut/test_mmseg_basic.cpp
+++ 3rdTools/mmseg/src/tests-ut/test_mmseg_basic.cpp
@@ -1,14 +1,41 @@
+#include <string>
 #include "MMSegTest.hpp"
 
 TEST_F(MMSegTest, basic) {
     css::Segmenter* seg = m_segmentManage.getSegmenter();
     ASSERT_TRUE(seg);
 
-    char input[] = "今天天气真好,我们干什么去呢";
+    char* tok;
 	u2 len = 0, symlen = 0;
+
+    char input[] = "中文分词测试";
     seg->setBuffer((u1*)input, sizeof(input));
 
-	char* tok = (char*)seg->peekToken(len, symlen);
-    printf("%s len=%d\n", tok, symlen);
-    //segment(out_file,seg);
+	tok = (char*)seg->peekToken(len, symlen);
+    ASSERT_TRUE(tok);
+    EXPECT_STREQ("中文", ::std::string(tok, symlen).c_str());
+	EXPECT_TRUE(!seg->isSentenceEnd());
+    seg->popToken(len, symlen);
+
+	tok = (char*)seg->peekToken(len, symlen);
+    ASSERT_TRUE(tok);
+    EXPECT_STREQ("分", ::std::string(tok, symlen).c_str());
+	EXPECT_TRUE(!seg->isSentenceEnd());
+    seg->popToken(len, symlen);
+
+	tok = (char*)seg->peekToken(len, symlen);
+    ASSERT_TRUE(tok);
+    EXPECT_STREQ("词", ::std::string(tok, symlen).c_str());
+	EXPECT_TRUE(!seg->isSentenceEnd());
+    seg->popToken(len, symlen);
+
+	tok = (char*)seg->peekToken(len, symlen);
+    ASSERT_TRUE(tok);
+    EXPECT_STREQ("测试", ::std::string(tok, symlen).c_str());
+	EXPECT_TRUE(!seg->isSentenceEnd());
+    seg->popToken(len, symlen);
+
+	tok = (char*)seg->peekToken(len, symlen);
+    ASSERT_TRUE(tok == NULL);
+	EXPECT_TRUE(seg->isSentenceEnd());
 }
