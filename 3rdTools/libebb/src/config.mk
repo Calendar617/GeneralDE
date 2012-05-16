9361b1010119e888d28aed483a48f6fc1c887954
Lokiwang <wangjian1009@gmail.com>
2012-05-16 14:23:35 +0800

    libebb compile success

--- 3rdTools/libebb/src/config.mk
+++ 3rdTools/libebb/src/config.mk
@@ -1,5 +1,5 @@
 product:=ebb
-$(product).type:=lib
+$(product).type:=ragel lib
 $(product).depends:=ev
 $(product).c.libraries:=
 $(product).version:=4.0.4
@@ -7,9 +7,9 @@ $(product).c.sources := $(addprefix $(product-base)/, \
                           ebb.c \
                           rbtree.c \
                        )
-
 $(product).product.c.includes:=3rdTools/libebb/include
-$(product).c.flags.cpp:=
+$(product).c.flags.cpp:=-Wno-pointer-to-int-cast -Wno-int-to-pointer-cast
 $(product).c.flags.ld:=
+$(product).ragel:=$(call def-ragel-to-c,$(product-base)/ebb_request_parser.rl)
 
 $(eval $(call product-def,$(product)))
